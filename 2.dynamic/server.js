628d6a9e9640467a1257d5979d19b59df0707956
zhufengnodejs <zhufengnodejs@126.com>
2016-09-06 17:41:16 +0800

    绘制界面

--- 2.dynamic/server.js
+++ 2.dynamic/server.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var routes = require('./routes');
+app.get('/',function(req,res){
+    //sendFile参数必须是一个绝对路径
+    res.sendFile(path.resolve('./index.html'));
+});
 /**
  * restful接口的特点是 路径相同，通过请求方法名来判断对此资源做如何的操作
  */
 routes(app);
-app.listen(80)
+app.listen(8080)
