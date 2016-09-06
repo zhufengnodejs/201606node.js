d88759e8c10f784950f743635db61983bdbe83cc
zhufengnodejs <zhufengnodejs@126.com>
2016-09-06 13:06:43 +0800

    事件

---
+++ 1.lesson/6.fs.js
@@ -0,0 +1,29 @@
+/**
+ * fs 重点方法
+ */
+var fs= require('fs');
+/*fs.readFile(); 读取文件
+fs.writeFile(); 写文件
+fs.exists(); 判断文件是否存在
+fs.mkdir(); 创建文件夹
+*/
+
+var path  = require('path');
+/*path.join();
+path.resolve();
+path.sep; window  linux mac 路径分隔符不一样
+
+*/
+console.log(path.sep);
+/**
+ * 级联创建目录
+ * 创建子文件夹的要求父文件是存在的
+ * 异步创建递归创建文件夹
+ */
+function mkdirp(dir){
+    /*fs.mkdir(dir,function(err){
+        console.log(err);
+    });*/
+}
+//console.log(path.join('a','b','c'));
+mkdirp(path.join('a','b','c'));
