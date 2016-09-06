bfcd7bffda5aad1cedcc2d2894906d17f5684b05
zhufengnodejs <zhufengnodejs@126.com>
2016-09-06 17:10:10 +0800

    fs & path

--- 2.dynamic/routes/index.js
+++ 2.dynamic/routes/index.js
@@ -3,7 +3,13 @@
  * 2. 对修改关闭，对扩展开放
  * @param app
  */
+var fs = require('fs');
 module.exports = function(app){
-   require('./user')(app);
-   require('./category')(app);
+   //因为当前目录是2.dynamic
+   var dirs = fs.readdirSync('./routes');
+   dirs.forEach(function(dir){
+      // 在加载模块的时候，.代表当前此模块所在的目录
+      if(dir != 'index.js')
+         require('./'+dir)(app);
+   });
 }
