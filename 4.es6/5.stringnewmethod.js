6bf5e0ab62c382b603c10e84366f7ce95a1d0de9
zhufengnodejs <zhufengnodejs@126.com>
2016-09-07 16:34:14 +0800

    自行实现reduce方法

--- 4.es6/5.stringnewmethod.js
+++ 4.es6/5.stringnewmethod.js
@@ -47,10 +47,38 @@ console.log(multiply(5));
 
 var nums = [1,2,3];
 console.log(nums.map((item,index)=>{
-    console.log(item*2);
+    console.log(index,item*2);
     return item*2;
 }));
+/*Array.prototype.from = function(args){
+  var newArr=[];
+  for(var i=0;i<args.length;i++)//类数组有长度和索引的key
+    newArr.push(args[i]);
+  return newArr;
+}*/
+/**
+ * 数组的新方法
+ */
+function sum(a,b){
+ //var args = Array.prototype.slice.call(arguments);
+ var args = Array.from(arguments);//把类数组转成数组
+ //return eval(args.join('+'));
+ //收敛 会一个数组经过计算得到一个唯一的值
+ return args.reduce(function(curr,item){
+    return curr + item;
+ },0);
+}
+console.log(sum(1,2,3,4,5));
 
+Array.prototype.reduce = function(reducer,initVal){
 
-
-
+}
+var args = [1,2,3];
+//把数组中的所有的元素依次传入函数中
+//第一次的时候 curr=初始值 item为数组中的第一个元素
+//第二次的时候 curr=上一次函数执行的返回值 item为数组中的第二个元素
+//第三次的时候 curr=上一次函数执行的返回值 item为数组中的第三个元素
+var result = args.reduce(function(curr,item){
+    return curr + item;
+},5);
+console.log(result);
