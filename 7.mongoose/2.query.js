95a5ee618644d3cc18c1666d5945ac1237668394
zhufengnodejs <zhufengnodejs@126.com>
2016-09-10 16:36:46 +0800

    批量保存数据

--- 7.mongoose/2.query.js
+++ 7.mongoose/2.query.js
@@ -9,7 +9,7 @@ var PersonSchema = new mongoose.Schema({
     age:{type:Number,default:0}
 });
 var PersonModel = mongoose.model('Person',PersonSchema);
-var users = [];
+/*var users = [];
 for(var i=1;i<=10;i++){
     users.push({name:'zfpx'+i,age:i});
 }//批量保存文档对象数组
@@ -17,6 +17,22 @@ PersonModel.create(users).then(function(data){
     console.log(data);
 },function(error){
     console.error(error);
+});*/
+
+
+/*PersonModel.findOne({age:5}).then(function(data){
+    console.log(data);
+});*/
+//将年龄等于5岁的年龄加5岁
+/*PersonModel.update({age:5},{$inc:{age:5}}).then(function(data){
+    console.log(data);
+});*/
+PersonModel.find({}).then(function(data){
+    console.log(data);
 });
-
-
+/*
+PersonModel.remove({name:{$exists:false}}).then(function(data){
+    console.log(data.result);
+},function(error){
+    console.error(error);
+});*/
