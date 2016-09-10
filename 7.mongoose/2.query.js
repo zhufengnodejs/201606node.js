2ce3410a9dc09fad9e87e3554e475e1c1c9ed841
zhufengnodejs <zhufengnodejs@126.com>
2016-09-10 17:23:03 +0800

    批量保存数据

--- 7.mongoose/2.query.js
+++ 7.mongoose/2.query.js
@@ -5,34 +5,66 @@ var mongoose = require('mongoose');
 mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/201606node');
 var PersonSchema = new mongoose.Schema({
-    name:String,
-    age:{type:Number,default:0}
+    name: String,
+    age: {type: Number, default: 0}
 });
-var PersonModel = mongoose.model('Person',PersonSchema);
-/*var users = [];
-for(var i=1;i<=10;i++){
-    users.push({name:'zfpx'+i,age:i});
-}//批量保存文档对象数组
-PersonModel.create(users).then(function(data){
-    console.log(data);
-},function(error){
-    console.error(error);
-});*/
+var PersonModel = mongoose.model('Person', PersonSchema);
+//var users = [];
+//for(var i=1;i<=10;i++){
+//    users.push({name:'zfpx'+i,age:i});
+//}//批量保存文档对象数组
+//PersonModel.create(users).then(function(data){
+//    console.log(data);
+//},function(error){
+//    console.error(error);
+//});
 
 
 /*PersonModel.findOne({age:5}).then(function(data){
-    console.log(data);
-});*/
+ console.log(data);
+ });*/
 //将年龄等于5岁的年龄加5岁
 /*PersonModel.update({age:5},{$inc:{age:5}}).then(function(data){
+ console.log(data);
+ });*/
+
+/*
+ PersonModel.remove({name:{$exists:false}}).then(function(data){
+ console.log(data.result);
+ },function(error){
+ console.error(error);
+ });*/
+
+/*PersonModel.findOne({},{name:0}).then(function(data){
+ console.log(data);
+ });*/
+//在mongoose里面操作_id都用字符串
+/*
+ PersonModel.findById('57d3c37b42b090139c2ccff1').then(function(data){
+ console.log(data);
+ });;*/
+//年龄等于 1 3 5
+/*PersonModel.find({$or:[{age:1},{age:3},{age:10}]}).then(function(data){
+ console.log(data);
+ });;*/
+/*
+ PersonModel.find({age:{$in:[1,3,10]}}).then(function(data){
+ console.log(data);
+ });;*/
+/**
+ *
+ */
+/*
+ PersonModel.remove({}).then(function(data){
+ console.log(data);
+ });;*/
+/*PersonModel.find({}).then(function(data){
     console.log(data);
 });*/
-PersonModel.find({}).then(function(data){
+
+var pageNum = 2;//当前页码
+var pageSize = 3;//每页的条数
+PersonModel.find({}).skip((pageNum - 1)*pageSize).limit(pageSize).sort({age: -1}).exec(function (err,data) {
     console.log(data);
 });
-/*
-PersonModel.remove({name:{$exists:false}}).then(function(data){
-    console.log(data.result);
-},function(error){
-    console.error(error);
-});*/
+//  7 6 5 4 3 2 1
---
+++ 7.mongoose/3.sort.js
@@ -0,0 +1,25 @@
+var Model = {
+    data:[2,1,3,4,6,5,7,8,9,10],//模拟数据库中的数据
+    skip(num){
+        this._skip = num;
+        return this;
+    },
+    limit(num){
+        this._limit = num;
+        return this;
+    },
+    sort(order){ // 1 升序 -1 降序
+        this._order = order;
+        return this;
+    },
+    exec(callback){
+        var page = this.data.sort((a,b)=>(a-b)*this._order)
+            .slice(this._skip,this._skip+this._limit);
+        callback(page);
+        return this;
+    }
+}
+
+Model.skip(3).limit(3).exec(function(data){
+    console.log(data);// 7 6 5
+}).sort(-1);
