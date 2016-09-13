46336a0335a393cd82d9100afbb9c6aae31c94f4
zhufengnodejs <zhufengnodejs@126.com>
2016-09-13 11:06:29 +0800

    如何使用类名和行内样式

--- 9.react/2.jsx.js
+++ 9.react/2.jsx.js
@@ -4,8 +4,9 @@
  * 遇到 {开头的话以js代码来解析
  */
 var names = ['张三','福原爱','张三'];
+var style = {color:'red'};
 ReactDOM.render(
-    <div>
+    <div className="bg" style={style}>
         {
           names.map((item,index)=><div key={index}>hello:{item}</div>)
         }
