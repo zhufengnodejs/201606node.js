2fe82104d393ddbd35a29c6ca8ca78f019c592e8
zhufengnodejs <zhufengnodejs@126.com>
2016-09-13 12:53:43 +0800

    setState是一个异步方法

--- 9.react/6.status.js
+++ 9.react/6.status.js
@@ -4,12 +4,21 @@ var Person = React.createClass({
         name:React.PropTypes.string.isRequired
    },
    //定义初始状态,可以通过this.state来引用这个状态对象
+    //状态是由内部初始化的，不能由外部直接改变
    getInitialState(){
         return {happy:true}
    },
     //处理点击事件,可以在此修改当前实例的状态，当状态改变之后会重新触发render方法，重新渲染视图
    handleClick(){
-     this.setState({happy:!this.state.happy});
+       var before = this.state;
+       console.log(this.state.happy);
+       // setState是一个异步方法，如果要取新的状态，必须在回调里面取
+       //不能在setstate方法之后取，因为异步方法要在所有的同步方法执行完毕之后执行
+     this.setState({happy:!this.state.happy},()=>{
+         console.log(this.state.happy);
+       /*  var after = this.state;
+         console.log(before === after );*/
+     });
    },
    render(){
        console.log('render');
