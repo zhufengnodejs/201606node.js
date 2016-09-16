a4e8797d08171c71ce151e339cad05c65b52f2d1
zhufengnodejs <zhufengnodejs@126.com>
2016-09-16 15:43:21 +0800

    实现了用户功能

--- 11.socket.io/2.chat.js
+++ 11.socket.io/2.chat.js
@@ -8,11 +8,19 @@ app.get('/',function(req,res){
 var server = require('http').createServer(app);
 
 var io = require('socket.io')(server);
+
 io.on('connection',function(socket){
+    var username;
     //向刚连接上的客户端发送消息
-    socket.send('欢迎来到珠峰聊天室');
+    socket.send({username:'系统',msg:'欢迎来到珠峰聊天室,请给自己起个呢称吧!'});
     socket.on('message',function(msg){
-        io.emit('message',msg);
+        if(username){
+            //把此消息广播给所有的人
+            io.emit('message',{username,msg});
+        }else{//此用户的第一次发言会作为此用户的用户名
+            username = msg;
+            socket.send({username:'系统',msg:`欢迎${username}来到聊天室`});
+        }
     });
 });
 
