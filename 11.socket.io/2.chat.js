d961fd42e875115e659500de6dc40238e64a773f
zhufengnodejs <zhufengnodejs@126.com>
2016-09-16 15:33:21 +0800

    实现客户端和服务器的聊天

--- 11.socket.io/2.chat.js
+++ 11.socket.io/2.chat.js
@@ -9,8 +9,10 @@ var server = require('http').createServer(app);
 
 var io = require('socket.io')(server);
 io.on('connection',function(socket){
+    //向刚连接上的客户端发送消息
+    socket.send('欢迎来到珠峰聊天室');
     socket.on('message',function(msg){
-
+        io.emit('message',msg);
     });
 });
 
