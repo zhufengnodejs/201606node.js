6f072e38c28a51d7dcf739a41fb383a83844d096
zhufengnodejs <zhufengnodejs@126.com>
2016-09-16 11:08:45 +0800

    长轮询

--- 9.websocket/3.iframe.js
+++ 9.websocket/3.iframe.js
@@ -9,10 +9,17 @@ var app = express();
 app.get('/',function(req,res){
   res.sendFile('./3.iframe.html',{root:__dirname});
 });
+//在子框架中通过parent可以引用父框架
+// 父框架 show    子框架的脚本里parent.show来引用父框架里的函数
 app.get('/time',function(req,res){
   setInterval(function(){
       //不能用end send 结束响应
-      res.write(new Date().toLocaleString());
+      //res.write(new Date().toLocaleString());
+      res.write(`
+      <script>
+         console.log('xxx');
+      </script>
+      `);
   },1000)
 });
 app.listen(9090);
