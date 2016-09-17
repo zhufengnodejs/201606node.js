882002f7724cd4ca80a70f66679394d718096fc5
zhufengnodejs <zhufengnodejs@126.com>
2016-09-17 16:35:34 +0800

    实现爬虫

--- 14.crawl/server.js
+++ 14.crawl/server.js
@@ -4,6 +4,7 @@
 var Movie = require('./db');
 var express = require('express');
 var path = require('path');
+
 var app = express();
 //指定模板的存放路径
 app.set('views',path.resolve('views'));
@@ -16,6 +17,15 @@ app.get('/',function(req,res){
         res.status(500).send(err);
     })
 });
+var CronJob = require('cron').CronJob;
+//每隔30分钟执行一次
+var job = new CronJob('0 */30 * * * *',function(){
+ var  child_process = require('child_process');
+ var child = child_process.spawn(process.execPath,['./tasks/main.js']);
+
+});
+job.start();
+
 
 
 app.listen(9090);
